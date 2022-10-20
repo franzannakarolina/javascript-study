bd01159c94a1b87827a6e78e2c10874051d96f75
franzannakarolina <franz.karols@gmail.com>
2022-10-19 22:13:25 -0300

    add objetos basicos

--- aula18/index.js
+++ aula18/index.js
@@ -1,21 +1,28 @@
-const pessoa1 = {
-  nome: 'Luiz',
-  sobrenome: 'Miranda',
-  idade: 25,
+// const pessoa1 = {
+//   nome: 'Luiz',
+//   sobrenome: 'Miranda',
+//   idade: 25,
 
-  fala() {
-    console.log(`A minha idade atual é ${this.idade}.`);
-  },
+//   fala() {
+//     console.log(`A minha idade atual é ${this.idade}.`);
+//   },
 
-  incrementaIdade() {
-    this.idade++;
-  }
-};
+//   incrementaIdade() {
+//     this.idade++;
+//   }
+// };
 
-pessoa1.fala();
-pessoa1.incrementaIdade();
-pessoa1.fala();
-pessoa1.incrementaIdade();
-pessoa1.fala();
-pessoa1.incrementaIdade();
-pessoa1.fala();
+// pessoa1.fala();
+// pessoa1.incrementaIdade();
+// pessoa1.fala();
+// pessoa1.incrementaIdade();
+// pessoa1.fala();
+// pessoa1.incrementaIdade();
+// pessoa1.fala();
+
+function criaPessoa(nome, sobrenome, idade) {
+  // parametro
+  return { nome, sobrenome, idade };
+}
+
+const pessoa1 = criaPessoa("Luiz", "Miranda", 25); // argumento é o valor que é passado para o parametro
